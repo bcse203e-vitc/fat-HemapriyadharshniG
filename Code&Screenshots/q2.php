1c7bf005c3a77fb76b24f88746845beaf66a74a5
HemapriyadharshniG <144695201+HemapriyadharshniG@users.noreply.github.com>
2025-11-14 15:36:47 +0530

    Add files via upload

---
+++ Code&Screenshots/q2.php
@@ -0,0 +1,26 @@
+<?php
+function normalizeText(fileName, mode){
+file.load("text.txt");
+$_SET="text.txt";
+if((read.$_SET==" " && mode=="expand"){
+read.$_SET="_";
+}
+if(read.$_SET=="_" && mode="expand"){
+read.$_SET=" ";
+}    
+}
+
+if(compress.onclick()){
+normalizeText(test.txt,compress);
+}
+if(expand.onclick()){
+normalizeText(test.txt,expand);
+}
+?>
+
+<html>
+<body>
+<button id="compress">Compress</button>
+<button id="expand">Expand</button>
+</body>
+</html>
